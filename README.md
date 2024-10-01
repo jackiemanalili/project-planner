1c9a7bd883020ae5cda42f7b3c7021648489d7ea
jackiemanalili <jackiemanalili@gmail.com>
2024-10-01 11:00:53 +0800

    removed unnecessary display

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 The project reads input from a file named `input.txt`
 This file should be placed inside `src/main/resources` folder
 **Limitation: All tasks that are depended upon should be declared first before the tasks that depend on them.
-##### input format
-Project: [Project Name]
+
+
+#### input format
+Project: [Project Name] <br>
 [TaskName], [Duration in number of days], [Task Dependencies separated by comma]
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -12,23 +12,9 @@ public class Main {
 
         String filePath = "src/main/resources/input.txt"; // Adjust the path as necessary
         try {
+
             List<Project> projects = Parser.parseProject(filePath);
-
-           // display input
-            for (Project project : projects) {
-                System.out.println("project:::: " + project.getName());
-                for (Task task : project.getTaskList()) {
-                    System.out.print("  task::: " + task.getName() + "---duration::::: " + task.getDurationInDays() + "---dependencies:::: ");
-                    for (Task dep : task.getDependencies()) {
-                        System.out.print(dep.getName() + " ");
-                    }
-                    System.out.println();
-                }
-            }
-
-            System.out.println("==================");
             ScheduleService.scheduleProjects(projects);
-
 
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
